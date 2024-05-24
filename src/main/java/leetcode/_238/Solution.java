4859fb68370ef4c914591c67c6aa34aad8424835
jaime.w <463954448@qq.com>
2024-05-24 19:01:46 -0400

    init

---
+++ src/main/java/leetcode/_238/Solution.java
@@ -0,0 +1,26 @@
+package leetcode._238;
+
+class Solution {
+
+  public int[] productExceptSelf(int[] nums) {
+    int n = nums.length, A[] = new int[n], B[] = new int[n];
+    for (int i = 0, j = n - 1; i < n; i++, j--) {
+      if (i == 0) {
+        A[i] = nums[i];
+        B[j] = nums[j];
+      } else {
+        A[i] = nums[i] * A[i - 1];
+        B[j] = nums[j] * B[j + 1];
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      int k = i == 0 ? 1 : A[i - 1], t = i == n - 1 ? 1 : B[i + 1];
+      nums[i] = k * t;
+    }
+    return nums;
+  }
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 4};
+    System.out.println(new Solution().productExceptSelf(nums));
+  }
+}
---
+++ src/main/java/neetcode/_150/Solution.java
@@ -0,0 +1,41 @@
+package neetcode._150;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+// Encode and Decode Strings
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s:strs) {
+            sb.append(s+"\n");
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        LinkedList<String> l = new LinkedList();
+        char[] cs = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for(char c:cs) {
+            if (c == '\n') {
+                l.add(sb.toString());
+                sb = new StringBuilder();
+            } else {
+                sb.append(c);
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = List.of("a", "b", "c");
+//        List<String> list = List.of("");
+        String encode = new Solution().encode(list);
+        System.out.println(encode);
+        System.out.println(new Solution().decode(encode));
+    }
+}
