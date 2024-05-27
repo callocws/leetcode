a01b4d70f0907576accc53afcf7e44a2becc87c4
jaime.w <463954448@qq.com>
2024-05-27 18:16:00 -0400

    init

---
+++ src/main/java/leetcode/_42/Solution.java
@@ -0,0 +1,26 @@
+package leetcode._42;
+
+class Solution {
+    public int trap(int[] height) {
+        int m = 0, h = 0;
+        for(int i = 0; i < height.length; i++) {
+            if(height[i] > height[h]) {
+                h = i;
+            }
+        }
+
+        for(int i = 0, l = 0; i < h; i++) {
+            int k = Math.min(l, height[h]) - height[i];
+            m += k < 0 ? 0 : k;
+            l = Math.max(height[i], l);
+        }
+
+        for(int i = height.length - 1, r = 0; i > h; i--) {
+            int k = Math.min(r, height[h]) - height[i];
+            m += k < 0 ? 0 : k;
+            r = Math.max(height[i], r);
+        }
+
+        return m;
+    }
+}
