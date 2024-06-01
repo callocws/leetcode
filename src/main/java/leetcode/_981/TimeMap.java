27a6e0dc32e3e24367adc24f8bcda7c7442f855a
jaime.w <463954448@qq.com>
2024-06-01 15:59:53 -0400

    init

---
+++ src/main/java/leetcode/_981/TimeMap.java
@@ -0,0 +1,60 @@
+package leetcode._981;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class TimeMap {
+    
+    HashMap<String, ArrayList<Data>> map = new HashMap();
+
+    class Data {
+        String value;
+        int timestamp;
+
+        public Data(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        ArrayList<Data> l = map.get(key);
+        if(l == null) {
+            l = new ArrayList();
+        }
+        l.add(new Data(value, timestamp));
+        map.put(key, l);
+    }
+    
+    public String get(String key, int timestamp) {
+        ArrayList<Data> l = map.get(key);
+        if(l == null) {
+            return "";
+        }
+        int i = 0, j = l.size() - 1;
+        for(; i <= j;) {
+            int m = (i + j) / 2;
+            Data d = l.get(m);
+            if(d.timestamp == timestamp) {
+                j = m;
+                break;
+            } else if(d.timestamp > timestamp){
+                j = m - 1;
+            } else {
+                i = m + 1;
+            }
+        }
+        return j < 0 ? "" : l.get(j).value;
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
