bccaa8dbb2576da494aef627a5f698b26509d6fe
jaime.w <463954448@qq.com>
2024-06-09 17:00:42 -0400

    init

--- src/main/java/leetcode/_206/Solution.java
+++ src/main/java/leetcode/_206/Solution.java
@@ -1,12 +1,8 @@
 package leetcode._206;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+
+import leetcode.ListNode;
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) {
