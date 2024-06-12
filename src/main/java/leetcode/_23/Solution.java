2d7a98aa99796633f0614d9c72b48517bf19ea46
jaime.w <463954448@qq.com>
2024-06-12 17:24:16 -0400

    init

--- src/main/java/leetcode/_23/Solution.java
+++ src/main/java/leetcode/_23/Solution.java
@@ -27,3 +27,36 @@ class Solution {
         return l.next;
     }
 }
+
+class Solution1 {
+
+    ListNode partition(ListNode[] lists, int i, int j) {
+        if(i >= j) {
+            return j < 0 ? null : lists[j];
+        }
+        int m = (i + j) / 2;
+        ListNode l = partition(lists, i, m);
+        ListNode r = partition(lists, m + 1, j);
+        return merge(l, r);
+    }
+
+    ListNode merge(ListNode p, ListNode q) {
+        if(p == null || q == null) {
+            return p == null ? q : p;
+        }
+        ListNode t;
+        if(p.val < q.val) {
+            t = merge(p.next, q);
+            p.next = t;
+            return p;
+        } else {
+            t = merge(p, q.next);
+            q.next = t;
+            return q;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partition(lists, 0, lists.length - 1);
+    }
+}
