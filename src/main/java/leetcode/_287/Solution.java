df53616df789aa9dfc2a4dce262b6fd4ffc96825
jaime.w <463954448@qq.com>
2024-06-11 20:28:52 -0400

    init

---
+++ src/main/java/leetcode/_287/Solution.java
@@ -0,0 +1,42 @@
+package leetcode._287;
+
+class Solution {
+
+  public int findDuplicate(int[] nums) {
+    int p = 0, q = nums[nums[0]], i = 1, j = 0;
+    for (; p != q; ) {
+      p = nums[p];
+      q = nums[nums[q]];
+    }
+    for (p = nums[p]; p != q; p = nums[p], i++) {
+    }
+    for (p = q = j = 0; j == 0 || p != q; ) {
+      if (j++ >= i) {
+        q = nums[q];
+      }
+      p = nums[p];
+    }
+    return p;
+  }
+}
+
+class Solution1 {
+
+  public int findDuplicate(int[] nums) {
+    int p = 0, q = 0;
+    do {
+      p = nums[p];
+      q = nums[nums[q]];
+    } while(p != q);
+
+    for(p = 0; p != q;) {
+      p = nums[p];
+      q = nums[q];
+    }
+    return p;
+  }
+  public static void main(String[] args) {
+    System.out.println(new Solution1().findDuplicate(new int[]{1, 3, 4, 2, 2}));
+    System.out.println(new Solution1().findDuplicate(new int[]{3, 3, 3, 3, 3}));
+  }
+}
