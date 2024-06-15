b679dfec977c7dc7f95c87c3e11f430fcb98075f
jaime.w <463954448@qq.com>
2024-06-15 16:56:11 -0400

    init

---
+++ src/main/java/leetcode/_98/Solution.java
@@ -0,0 +1,32 @@
+package leetcode._98;
+
+import leetcode.TreeNode;
+
+class Solution {
+    
+    boolean b = true;
+    public boolean isValidBST(TreeNode root) {
+        f(root);
+        return b;
+    }
+
+    int[] f(TreeNode p) {
+        if(p == null) {
+            return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE};
+        }
+        int[] l = f(p.left), r = f(p.right);
+        if(!b || !((p.left == null || l[1] < p.val) && (p.right == null || p.val < r[0]))) {
+            b = false;
+            return new int[]{-1, -1};
+        }
+        return new int[] {p.left == null ? p.val : l[0], p.right == null ? p.val : r[1]};
+    }
+
+    public static void main(String[] args) {
+        // [2,1,3]
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(new Solution().isValidBST(root));
+    }
+}
