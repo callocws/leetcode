78a5488c4199a2b673839b31888148f8d3a70967
jaime.w <463954448@qq.com>
2024-06-16 14:58:46 -0400

    init

---
+++ src/main/java/leetcode/_230/Solution.java
@@ -0,0 +1,46 @@
+package leetcode._230;
+
+import leetcode.TreeNode;
+
+class Solution {
+    int i = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        return f(root, k);
+    }
+
+    int f(TreeNode p, int k) {
+        if(p == null) {
+            return -1;
+        }
+        int t = f(p.left, k);
+        if(t >= 0) {
+            return t;
+        }
+        if(++i == k) {
+            return p.val;
+        }
+        t = f(p.right, k);
+        return t >= 0? t : -1;
+    }
+}
+
+class Solution1 {
+    int i = 0;
+    int cnt = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        f(root, k);
+        return cnt;
+    }
+
+    void f(TreeNode p, int k) {
+        if(p == null) {
+            return;
+        }
+        f(p.left, k);
+        if(++i == k) {
+            cnt = p.val;
+            return;
+        }
+        f(p.right, k);
+    }
+}
