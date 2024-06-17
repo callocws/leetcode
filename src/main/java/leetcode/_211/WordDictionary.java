68cc3128cbbf61412ec32eca5e603515a45d105c
jaime.w <463954448@qq.com>
2024-06-17 15:59:14 -0400

    init

--- src/main/java/leetcode/_211/WordDictionary.java
+++ src/main/java/leetcode/_211/WordDictionary.java
@@ -50,6 +50,56 @@ class WordDictionary {
     }
 }
 
+class WordDictionary1 {
+
+    public class Node {
+        Node[] ns = new Node[128];
+        boolean isComplete;
+    }
+    Node head = new Node();
+    public WordDictionary1() {
+
+    }
+
+    public void addWord(String word) {
+        Node p = head;
+        for(char c : word.toCharArray()) {
+            Node t = p.ns[c];
+            if(t == null) {
+                t = new Node();
+            }
+            p.ns[c] = t;
+            p = t;
+        }
+        p.isComplete = true;
+    }
+
+    public boolean search(String word) {
+        return search(head, word);
+    }
+
+    boolean search(Node p, String word) {
+        char cs[] = word.toCharArray();
+        for(int i = 0; i < cs.length; i++) {
+            char c = cs[i];
+            if(c == '.') {
+                for(int j = 0; j < 128; j++) {
+                    if(p.ns[j] != null && search(p.ns[j], word.substring(i + 1))) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            Node t = p.ns[c];
+            if(t == null) {
+                return false;
+            }
+            p = t;
+        }
+        return p.isComplete;
+    }
+}
+
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary obj = new WordDictionary();
