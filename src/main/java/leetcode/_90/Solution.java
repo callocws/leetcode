566d80f47a4418378a0ba7944aea97864196142a
jaime.w <463954448@qq.com>
2024-06-19 14:07:10 -0400

    init

--- src/main/java/leetcode/_90/Solution.java
+++ src/main/java/leetcode/_90/Solution.java
@@ -22,3 +22,24 @@ class Solution {
         }
     }
 }
+
+class Solution1 {
+    LinkedList<List<Integer>> l = new LinkedList();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        traverse(new LinkedList(), nums, 0);
+        return l;
+    }
+    void traverse(List<Integer> r, int[] A, int i) {
+        l.add(new LinkedList(r));
+        for(int j = i; j < A.length; j++) {
+            if(j > i && A[j] == A[j - 1]) {
+                continue;
+            }
+            r.add(A[j]);
+            traverse(r, A, j + 1);
+            r.removeLast();
+        }
+    }
+}
