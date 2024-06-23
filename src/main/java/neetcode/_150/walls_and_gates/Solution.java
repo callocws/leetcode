dbee28faead14af4794987b660d6db0970cf154c
jaime.w <463954448@qq.com>
2024-06-23 18:09:16 -0400

    init

--- src/main/java/neetcode/_150/walls_and_gates/Solution.java
+++ src/main/java/neetcode/_150/walls_and_gates/Solution.java
@@ -22,7 +22,7 @@ class Solution {
         if (A[i][j] < n) {
             return;
         }
-        
+
         int t = Math.min(A[i][j], n);
         A[i][j] = -1;
         n++;
@@ -62,4 +62,28 @@ class Solution {
         System.out.println(Arrays.deepToString(grid));
     }
 
+}
+
+
+class Solution1 {
+    public void wallsAndGates(int[][] grid) {
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 0) {
+                    traverse(grid, i, j, 0);
+                }
+            }
+        }
+    }
+
+    void traverse(int[][] A, int i, int j, int n) {
+        if(i < 0 || j < 0 || i >= A.length || j >= A[0].length || A[i][j] == -1 || n != 0 && A[i][j] <= n) {
+            return;
+        }
+        A[i][j] = n++;
+        traverse(A, i - 1, j, n);
+        traverse(A, i, j - 1, n);
+        traverse(A, i + 1, j, n);
+        traverse(A, i, j + 1, n);
+    }
 }
