3219f66ee1873262c8ee6e6fa6695e7b7738a8fa
jaime.w <463954448@qq.com>
2024-06-25 17:22:47 -0400

    init

--- src/main/java/leetcode/_207/Solution.java
+++ src/main/java/leetcode/_207/Solution.java
@@ -2,6 +2,7 @@ package leetcode._207;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 class Solution {
@@ -38,5 +39,47 @@ class Solution {
             ns.addAll(n.s);
         }
         return check(p, ns);
+    }
+}
+
+class Solution1 {
+    class Node {
+        int v;
+        int d;
+        Set<Node> s = new HashSet();
+        public Node(int v) {
+            this.v = v;
+        }
+    }
+    HashMap<Integer, Node> m = new HashMap();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for(int i = 0; i < numCourses; i++) {
+            m.put(i, new Node(i));
+        }
+        for(int i = 0; i < prerequisites.length; i++) {
+            Node a = m.get(prerequisites[i][0]), b = m.get(prerequisites[i][1]);
+            b.s.add(a);
+            a.d++;
+        }
+        LinkedList<Node> l = new LinkedList();
+        for(int i = 0; i < numCourses; i++) {
+            if(m.get(i).d == 0) {
+                l.add(m.get(i));
+            }
+        }
+        for(;!l.isEmpty();) {
+            Node p = l.pop();
+            for(Node n : p.s) {
+                if(--n.d == 0) {
+                    l.add(n);
+                }
+            }
+        }
+        for(int i = 0; i < numCourses; i++) {
+            if(m.get(i).d != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
