a9c99ffe4916622cc3ed1a4dd9f1c70fb20e7b11
jaime.w <463954448@qq.com>
2024-06-26 17:19:20 -0400

    init

---
+++ src/main/java/leetcode/_684/Solution.java
@@ -0,0 +1,41 @@
+package leetcode._684;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        HashMap<Integer, ArrayList<Integer>> m = new HashMap();
+        HashSet<Integer> visited = new HashSet();
+        for(int[] e : edges) {
+            addEdge(m, e[0], e[1]);
+            addEdge(m, e[1], e[0]);
+            visited.clear();
+            if(hasCycle(m, visited, e[0], -1)) {
+                return e;
+            }
+        }
+        return null;
+    }
+    void addEdge(HashMap<Integer, ArrayList<Integer>> m, int i, int j) {
+        ArrayList<Integer> arr = m.get(i);
+        if (arr == null) {
+            arr = new ArrayList();
+        }
+        arr.add(j);
+        m.put(i, arr);
+    }
+    boolean hasCycle(HashMap<Integer, ArrayList<Integer>> m, HashSet<Integer> visited, int i, int pre) {
+        if(visited.contains(i)) {
+            return true;
+        }
+        visited.add(i);
+        for(int j : m.get(i)) {
+            if(j != pre && hasCycle(m, visited, j, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
