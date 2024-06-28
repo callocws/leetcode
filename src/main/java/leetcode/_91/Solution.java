c389a188fa305aae33783240ad7d934556658710
jaime.w <463954448@qq.com>
2024-06-28 16:23:49 -0400

    init

--- src/main/java/leetcode/_91/Solution.java
+++ src/main/java/leetcode/_91/Solution.java
@@ -24,3 +24,22 @@ class Solution {
         return b;
     }
 }
+
+class Solution1 {
+    public int numDecodings(String s) {
+        int a, b, i;
+        char[] cs = s.toCharArray();
+        for(a = i = 0, b = 1; i < cs.length; i++) {
+            int t = 0;
+            if(i > 0 && ('1' == cs[i - 1] || cs[i - 1] == '2' && cs[i] <= '6')) {
+                t += a;
+            }
+            if('1' <= cs[i] && cs[i] <= '9') {
+                t += b;
+            }
+            a = b;
+            b = t;
+        }
+        return b;
+    }
+}
