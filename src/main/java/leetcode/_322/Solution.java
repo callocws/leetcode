67f84f3df7b2da0859f4b9cdcd637eb20e5a089e
jaime.w <463954448@qq.com>
2024-06-28 18:16:35 -0400

    init

--- src/main/java/leetcode/_322/Solution.java
+++ src/main/java/leetcode/_322/Solution.java
@@ -23,3 +23,20 @@ class Solution {
         return amount == 0 ? 0 : A[amount] == 0 ? -1 : A[amount];
     }
 }
+
+
+class Solution1 {
+    public int coinChange(int[] coins, int amount) {
+        int[] A = new int[amount + 1];
+        for(int i = 1; i < A.length; i++) {
+            A[i] = Integer.MAX_VALUE;
+            for(int j = 0; j < coins.length; j++) {
+                int x = i - coins[j];
+                if(x >= 0 && A[x] < Integer.MAX_VALUE) {
+                    A[i] = Math.min(A[i], 1 + A[x]);
+                }
+            }
+        }
+        return amount == 0 ? 0 : A[amount] == Integer.MAX_VALUE ? -1 : A[amount];
+    }
+}
