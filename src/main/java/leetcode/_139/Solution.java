ac6ed025c317121ba0c77f9e3ed643afb9275126
jaime.w <463954448@qq.com>
2024-06-29 16:40:05 -0400

    init

--- src/main/java/leetcode/_139/Solution.java
+++ src/main/java/leetcode/_139/Solution.java
@@ -1,6 +1,7 @@
 package leetcode._139;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 class Solution {
@@ -44,7 +45,38 @@ class Solution {
         }
         return root.get(s, root);
     }
+
+    public static void main(String[] args) {
+        // s =
+        //"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+        //wordDict =
+        //["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+        System.out.println(new Solution().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+                List.of("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")));
+    }
 }
+
+
+class Solution1 {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet(wordDict);
+        return contains(s, set);
+    }
+
+    boolean contains(String s, HashSet<String> set) {
+        if(s.isEmpty()) {
+            return true;
+        }
+        for(int i = 1; i <= s.length(); i++) {
+            if(set.contains(s.substring(0, i)) && contains(s.substring(i), set)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 
 
 
