c2155e4c7fa22a84081179e451b3c774709ec9f4
jaime.w <463954448@qq.com>
2024-06-30 17:16:55 -0400

    init

---
+++ src/main/java/leetcode/_57/Solution.java
@@ -0,0 +1,38 @@
+package leetcode._57;
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if(intervals.length == 0) {
+            return new int[][]{newInterval};
+        }
+        ArrayList<int[]> arr1 = new ArrayList(), arr2 = new ArrayList();
+        boolean first = true;
+        for(int[] inter : intervals) {
+            if(first && inter[0] >= newInterval[0]) {
+                arr1.add(newInterval);
+                first = false;
+            }
+            arr1.add(inter);
+        }
+        if(first) {
+            arr1.add(newInterval);
+        }
+        arr2.add(arr1.removeFirst());
+        for(int[] t : arr1) {
+            int[] p = arr2.getLast();
+            if(p[1] >= t[0]) {
+                arr2.removeLast();
+                arr2.add(new int[]{Math.min(p[0], t[0]), Math.max(p[1], t[1])});
+            } else {
+                arr2.add(t);
+            }
+        }
+        int[][] r = new int[arr2.size()][2];
+        for(int i = 0; i < r.length; i++) {
+            r[i] = arr2.get(i);
+        }
+        return r;
+    }
+}
