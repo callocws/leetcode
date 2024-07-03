c526834317f862f8138dfc5e0234676ccf1fe284
jaime.w <463954448@qq.com>
2024-07-03 13:05:42 -0400

    init

--- src/main/java/leetcode/_134/Solution.java
+++ src/main/java/leetcode/_134/Solution.java
@@ -23,3 +23,24 @@ class Solution {
         return -1;
     }
 }
+
+class Solution1 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int i, j, n = gas.length, s = 0, cnt = 0, index = 0;
+        for (i = 0; i < n; i++) {
+            gas[i] -= cost[i];
+        }
+        for(j = i = 0; i < 2 * n; i++) {
+            s += gas[j];
+            j = (j + 1) % n;
+            if(s < 0) {
+                s = 0;
+                cnt = 0;
+                index = j;
+            } else {
+                cnt++;
+            }
+        }
+        return cnt >= n ? index : -1;
+    }
+}
