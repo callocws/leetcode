86ea76a15aacb5eb8d367fca37405b536ae0e59f
jaime.w <463954448@qq.com>
2024-07-03 17:22:13 -0400

    init

--- src/main/java/leetcode/_678/Solution.java
+++ src/main/java/leetcode/_678/Solution.java
@@ -21,3 +21,29 @@ class Solution {
         return  f(cs, i + 1, n) || f(cs, i + 1, n - 1) || f(cs, i + 1, n + 1);
     }
 }
+
+class Solution1 {
+    public boolean checkValidString(String s) {
+        char cs[] = s.toCharArray();
+        boolean dp[][] = new boolean[cs.length + 1][cs.length + 1];
+        dp[0][0] = true;
+        for(int i = 0; i < dp.length - 1; i++) {
+            for(int j = 0; j < dp.length; j++) {
+                if(dp[i][j]) {
+                    if(cs[i] == '(') {
+                        dp[i + 1][j + 1] = true;
+                    } else if(cs[i] == ')' && j - 1 >= 0) {
+                        dp[i + 1][j - 1] = true;
+                    } else if(cs[i] == '*'){
+                        dp[i + 1][j] = true;
+                        if(j - 1 >= 0) {
+                            dp[i + 1][j - 1] = true;
+                        }
+                        dp[i + 1][j + 1] = true;
+                    }
+                }
+            }
+        }
+        return dp[cs.length][0];
+    }
+}
