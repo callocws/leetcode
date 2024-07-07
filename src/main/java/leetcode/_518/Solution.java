1c2718486ed6fde34bc3274ebd9039e6fd9b5079
jaime.w <463954448@qq.com>
2024-07-07 18:40:10 -0400

    init

--- src/main/java/leetcode/_518/Solution.java
+++ src/main/java/leetcode/_518/Solution.java
@@ -1,7 +1,35 @@
 package leetcode._518;
 
-// wrong, it calculates a duplicate number of ways to make change with the coins
+import java.util.HashMap;
+
 class Solution {
+    HashMap<String, Integer> m = new HashMap<>();
+    public int change(int amount, int[] coins) {
+        return calculate(0, amount, coins);
+    }
+
+    int calculate(int index, int target, int[] coins) {
+        if(target == 0) {
+            return 1;
+        }
+        if(index >= coins.length) {
+            return 0;
+        }
+        String key = index + " " + target;
+        if (m.containsKey(key)) {
+            return m.get(key);
+        }
+        int s = 0;
+        for(int i = 0; i * coins[index] <= target; i++) {
+            s += calculate(index + 1, target - i * coins[index], coins);
+        }
+        m.put(key, s);
+        return s;
+    }
+}
+
+// wrong, it calculates a duplicate number of ways to make change with the coins
+class Solutionx {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
         for(int i = 0; i < coins.length; i++) {
@@ -16,6 +44,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().change(5, new int[]{1, 2, 5}));
+        System.out.println(new Solutionx().change(5, new int[]{1, 2, 5}));
     }
 }
