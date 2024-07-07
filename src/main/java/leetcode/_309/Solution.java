566d7aadc399d44518a6787a6706b96cd55fb394
jaime.w <463954448@qq.com>
2024-07-07 17:14:53 -0400

    init

--- src/main/java/leetcode/_309/Solution.java
+++ src/main/java/leetcode/_309/Solution.java
@@ -1,5 +1,7 @@
 package leetcode._309;
 
+import java.util.HashMap;
+
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length, s[][] = new int[3][n + 1], i = 1;
@@ -11,3 +13,34 @@ class Solution {
         return Math.max(s[0][n], s[2][n]);
     }
 }
+
+class Solution1 {
+    int n;
+    int[] prices;
+    HashMap<String, Integer> m = new HashMap();
+    public int maxProfit(int[] prices) {
+        n = prices.length;
+        this.prices = prices;
+        return f(0, true);
+    }
+
+    int f(int i, boolean buyavail) {
+        if(i >= n) {
+            return 0;
+        }
+        String key = ""+i+buyavail;
+        if(m.containsKey(key)) {
+            return m.get(key);
+        }
+        int c = f(i + 1, buyavail), t;
+        if(buyavail) {
+            t = f(i + 1, false) - prices[i];
+        } else {
+            t = f(i + 2, true) + prices[i];
+        }
+        int v = Math.max(c, t);
+        m.put(key, v);
+        return v;
+    }
+}
+
