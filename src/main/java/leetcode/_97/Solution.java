f78d1efd19e617ab46b839e53720273f73e582c8
jaime.w <463954448@qq.com>
2024-07-08 14:04:24 -0400

    init

--- src/main/java/leetcode/_97/Solution.java
+++ src/main/java/leetcode/_97/Solution.java
@@ -23,3 +23,27 @@ class Solution {
         return false;
     }
 }
+
+class Solution1 {
+    Boolean[][] dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        dp = new Boolean[s1.length() + 1][s2.length() + 1];
+        return s1.length() + s2.length() == s3.length() && isInterleave(s1, s2, s3, 0, 0);
+    }
+
+    boolean isInterleave(String s1, String s2, String s3, int i1, int i2) {
+        if(i1 + i2 == s3.length()) {
+            return true;
+        }
+        if(dp[i1][i2] != null) {
+            return dp[i1][i2];
+        }
+        if(i1 < s1.length() && s3.charAt(i1 + i2) == s1.charAt(i1) && isInterleave(s1, s2, s3, i1 + 1, i2)) {
+            return true;
+        } else if(i2 < s2.length() && s3.charAt(i1 + i2) == s2.charAt(i2) && isInterleave(s1, s2, s3, i1, i2 + 1)) {
+            return true;
+        }
+        dp[i1][i2] = false;
+        return false;
+    }
+}
