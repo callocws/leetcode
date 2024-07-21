47dfadce804bf9d16f4feb31ad419a3133176efe
jaime.w <463954448@qq.com>
2024-07-21 15:55:17 -0400

    init

--- src/main/java/leetcode/_560/Solution.java
+++ src/main/java/leetcode/_560/Solution.java
@@ -7,7 +7,7 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, ArrayList<Integer>> m = new HashMap();
         int prefixSum[] = new int[nums.length + 1], i = 1, cnt = 0;
-        for(prefixSum[0] = nums[0]; i <= nums.length; i++) {
+        for(; i <= nums.length; i++) {
             prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
             m.computeIfAbsent(prefixSum[i], key -> new ArrayList()).add(i);
         }
@@ -23,6 +23,24 @@ class Solution {
                     j++;
                 }
             }
+        }
+        return cnt;
+    }
+}
+
+class Solution1 {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, ArrayList<Integer>> m = new HashMap();
+        int prefixSum[] = new int[nums.length + 1], i = 0, cnt = 0;
+        for(; i <= nums.length; i++) {
+            if(i > 0) {
+                prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
+            }
+            ArrayList<Integer> indexs = m.get(prefixSum[i] - k);
+            if(indexs != null) {
+                cnt += indexs.size();
+            }
+            m.computeIfAbsent(prefixSum[i], key -> new ArrayList()).add(i);
         }
         return cnt;
     }
