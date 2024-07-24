57dbaf991dbc306438929a35c37cd09789aed1fb
jaime.w <463954448@qq.com>
2024-07-24 14:28:15 -0400

    init

--- src/main/java/leetcode/_1258/Solution.java
+++ src/main/java/leetcode/_1258/Solution.java
@@ -1,6 +1,7 @@
 package leetcode._1258;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -49,3 +50,70 @@ class Solution {
 //        System.out.println(s.generateSentences(List.of(List.of("happy", "joy"), List.of("cheerful", "glad")), "I am happy today but was sad yesterday"));
     }
 }
+
+class Solution1 {
+    ArrayList<String> result = new ArrayList();
+    HashMap<String, String> map = new HashMap();
+    HashMap<String, HashSet<String>> mapSet = new HashMap();
+    public List<String> generateSentences(List<List<String>> synonyms, String text) {
+        for(List<String> strs : synonyms) {
+            for(String str : strs) {
+                map.put(str, str);
+            }
+        }
+        for(List<String> strs : synonyms) {
+            add(strs.get(0), strs.get(1));
+        }
+        for(List<String> strs : synonyms) {
+            for(String str : strs) {
+                mapSet.computeIfAbsent(findParent(str), k -> new HashSet<String>()).add(str);
+            }
+        }
+
+        generateSentences(0, text.split(" "));
+        result.sort(null);
+        return result;
+    }
+
+    String findParent(String str) {
+        String parent = map.get(str);
+        if(parent == null || parent == str) {
+            return parent;
+        }
+        parent = findParent(parent);
+        map.put(str, parent);
+        return parent;
+    }
+
+    void add(String a, String b) {
+        String parent1 = findParent(a);
+        String parent2 = findParent(b);
+        map.put(a, parent2);
+    }
+
+    void generateSentences(int index, String[] words) {
+        if(index == words.length) {
+            result.add(String.join(" ", words));
+            return;
+        }
+        String parent = findParent(words[index]);
+        if(parent != null) {
+            for(var word : mapSet.get(findParent(words[index]))) {
+                words[index] = word;
+                generateSentences(index + 1, words);
+            }
+            return;
+        }
+        generateSentences(index + 1, words);
+    }
+
+    public static void main(String[] args) {
+        var s = new Solution1();
+        System.out.println(s.generateSentences(List.of(List.of("happy", "joy"), List.of("sad", "sorrow"), List.of("joy", "cheerful")), "I am happy today but was sad yesterday"));
+
+
+//        System.out.println(s.generateSentences(List.of(List.of("happy", "joy"), List.of("cheerful", "glad")), "I am happy today but was sad yesterday"));
+    }
+
+}
+
