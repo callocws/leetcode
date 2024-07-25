90a75af73f86dec0b9875ae77fcaf55508b0c449
jaime.w <463954448@qq.com>
2024-07-25 13:48:13 -0400

    init

--- src/main/java/leetcode/_190/Solution.java
+++ src/main/java/leetcode/_190/Solution.java
@@ -2,11 +2,23 @@ package leetcode._190;
 
 public class Solution {
     public int reverseBits(int n) {
-        for(int i = 0; i < 15; i++) {
+        for(int i = 0; i <= 15; i++) {
             int h = (n >>> (31 - i)) & 1, l = (n >>> i) & 1;
             n &= ~(1 << i) & ~(1 << (31 - i));
             n |= (h << i) | (l << (31 - i));
         }
         return n;
+    }
+}
+
+class Solution1 {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+        }
+        return res;
     }
 }
