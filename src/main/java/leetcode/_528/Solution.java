40b5e24654336a58d4dde92b66ee79a3642c1403
jaime.w <463954448@qq.com>
2024-08-02 15:05:50 -0400

    init

---
+++ src/main/java/leetcode/_528/Solution.java
@@ -0,0 +1,55 @@
+package leetcode._528;
+
+import java.util.Random;
+
+class Solution {
+    int size;
+    Random r;
+    int[] w;
+    public Solution(int[] w) {
+        size = w.length;
+        for(int i = 1; i < size; i++) {
+            w[i] += w[i - 1];
+        }
+        r = new Random();
+        this.w = w;
+    }
+    
+    public int pickIndex() {
+        int v = r.nextInt(w[size - 1]);
+        for(int i = 0; i < size; i++) {
+            if(v < w[i]) {
+                return i;
+            }
+        }
+        return size - 1;
+    }
+}
+
+class Solution1 {
+    int size;
+    Random r;
+    int[] w;
+    public Solution1(int[] w) {
+        size = w.length;
+        for(int i = 1; i < size; i++) {
+            w[i] += w[i - 1];
+        }
+        r = new Random();
+        this.w = w;
+    }
+
+    public int pickIndex() {
+        int v = r.nextInt(w[size - 1]);
+        int m, i, j;
+        for(i = 0, j = size - 1; i < j;) {
+            m = (i + j) >> 1;
+            if(v < w[m]) {
+                j = m;
+            } else {
+                i = m + 1;
+            }
+        }
+        return i;
+    }
+}
