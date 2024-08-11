323a5394dadd1c916b9d7db7eb44602a08288240
jaime.w <463954448@qq.com>
2024-08-11 13:54:59 -0400

    init

--- src/main/java/leetcode/_167/Solution.java
+++ src/main/java/leetcode/_167/Solution.java
@@ -6,14 +6,30 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> m = new HashMap();
-        for(int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             m.put(numbers[i], i + 1);
         }
 
-        for(int i = 0; i < numbers.length; i++) { // can combine the logic together
+        for (int i = 0; i < numbers.length; i++) {
             Integer v = m.get(target - numbers[i]);
-            if(v != null) {
-                return new int[]{i + 1, v};
+            if (v != null) {
+                return new int[] {i + 1, v};
+            }
+        }
+        return null;
+    }
+}
+
+class Solution1 {
+    public int[] twoSum(int[] numbers, int target) {
+        for (int l = 0, r = numbers.length - 1; l < r; ) {
+            int s = numbers[l] + numbers[r];
+            if (s == target) {
+                return new int[] {l + 1, r + 1};
+            } else if (s < target) {
+                l++;
+            } else {
+                r--;
             }
         }
         return null;
