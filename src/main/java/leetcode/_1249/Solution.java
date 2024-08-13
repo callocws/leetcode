4093da4f78b5f5a5dc3b381281fae07da9ad38bb
jaime.w <463954448@qq.com>
2024-08-13 14:18:08 -0400

    init

--- src/main/java/leetcode/_1249/Solution.java
+++ src/main/java/leetcode/_1249/Solution.java
@@ -6,23 +6,23 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         int cnt = 0;
         StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()) {
-            if(c == '(') {
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 cnt++;
-            } 
-            if(c == ')') {
-             if(cnt == 0) {
-                continue;
-             } else {
-                cnt--;
-             }
-            } 
+            }
+            if (c == ')') {
+                if (cnt == 0) {
+                    continue;
+                } else {
+                    cnt--;
+                }
+            }
             sb.append(c);
         }
         char cs[] = sb.toString().toCharArray();
         s = "";
-        for(int i = cs.length - 1; i >= 0; i--) {
-            if(cnt > 0 && cs[i] == '(') {
+        for (int i = cs.length - 1; i >= 0; i--) {
+            if (cnt > 0 && cs[i] == '(') {
                 cnt--;
                 continue;
             }
@@ -32,31 +32,53 @@ class Solution {
     }
 }
 
-
 class Solution1 {
     public String minRemoveToMakeValid(String s) {
         StringBuilder sb = new StringBuilder();
         LinkedList<Integer> l = new LinkedList();
         char cs[] = s.toCharArray();
-        for(int i = 0; i < cs.length; i++) {
-            if(cs[i] == '(') {
+        for (int i = 0; i < cs.length; i++) {
+            if (cs[i] == '(') {
                 l.push(i);
             }
-            if(cs[i] == ')') {
-                if(l.isEmpty()) {
+            if (cs[i] == ')') {
+                if (l.isEmpty()) {
                     cs[i] = '.';
                 } else {
                     l.pop();
                 }
             }
         }
-        for(int index : l) {
+        for (int index : l) {
             cs[index] = '.';
         }
-        for(char c : cs) {
-            if(c != '.') {
+        for (char c : cs) {
+            if (c != '.') {
                 sb.append(c);
             }
+        }
+        return sb.toString();
+    }
+}
+
+class Solution2 {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        LinkedList<Integer> l = new LinkedList();
+        char cs[] = s.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+            if (!l.isEmpty() && cs[l.getLast()] == '(' && cs[i] == ')') {
+                l.removeLast();
+            } else if (cs[i] == '(' || cs[i] == ')') {
+                l.add(i);
+            }
+        }
+        for (int i = 0; i < cs.length; i++) {
+            if (!l.isEmpty() && l.getFirst() == i) {
+                l.removeFirst();
+                continue;
+            }
+            sb.append(cs[i]);
         }
         return sb.toString();
     }
