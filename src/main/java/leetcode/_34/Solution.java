cf9e40bf18edf489d80f42e15e68622d4fb11e8f
jaime.w <463954448@qq.com>
2024-08-16 15:51:40 -0400

    init

--- src/main/java/leetcode/_34/Solution.java
+++ src/main/java/leetcode/_34/Solution.java
@@ -1,20 +1,19 @@
 package leetcode._34;
 
-
-
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = search(nums, 0, nums.length, target), r = search(nums, 0, nums.length, target + 1) - 1;
-        if(l == nums.length || nums[l] != target) {
-            return new int[]{-1, -1};
+        int l = search(nums, 0, nums.length, target),
+                r = search(nums, 0, nums.length, target + 1) - 1;
+        if (l == nums.length || nums[l] != target) {
+            return new int[] {-1, -1};
         }
-        return new int[]{l, r};
+        return new int[] {l, r};
     }
 
     int search(int[] A, int i, int j, int target) {
-        for(; i < j; ) {
+        for (; i < j; ) {
             int m = (i + j) >> 1;
-            if(A[m] < target) {
+            if (A[m] < target) {
                 i = m + 1;
             } else {
                 j = m;
@@ -24,25 +23,57 @@ class Solution {
     }
 }
 
+class Solution1 {
+    public int[] searchRange(int[] nums, int target) {
+        int l = search(nums, 0, nums.length, target), r = search1(nums, 0, nums.length, target);
+        if (l == nums.length || nums[l] != target) {
+            return new int[] {-1, -1};
+        }
+        return new int[] {l, r};
+    }
 
+    int search(int[] A, int i, int j, int target) {
+        for (; i < j; ) {
+            int m = (i + j) >> 1;
+            if (A[m] < target) {
+                i = m + 1;
+            } else {
+                j = m;
+            }
+        }
+        return i;
+    }
+
+    int search1(int[] A, int i, int j, int target) {
+        for (; i < j; ) {
+            int m = (i + j) >> 1;
+            if (A[m] <= target) {
+                i = m + 1;
+            } else {
+                j = m;
+            }
+        }
+        return i - 1;
+    }
+}
 
 class Solution2 {
     public int[] searchRange(int[] nums, int target) {
         int l = search(nums, 0, nums.length, target), r = search1(nums, 0, nums.length, target);
-        if(l == nums.length || nums[l] != target) {
-            return new int[]{-1, -1};
+        if (l == nums.length || nums[l] != target) {
+            return new int[] {-1, -1};
         }
-        return new int[]{l, r};
+        return new int[] {l, r};
     }
 
     int search(int[] A, int i, int j, int target) {
-        if(i >= j) {
+        if (i >= j) {
             return i;
         }
         int m = (i + j) >> 1;
-        if(A[m] == target) {
+        if (A[m] == target) {
             return search(A, i, m, target);
-        } else if(A[m] < target) {
+        } else if (A[m] < target) {
             return search(A, m + 1, j, target);
         } else {
             return search(A, i, m, target);
@@ -50,13 +81,13 @@ class Solution2 {
     }
 
     int search1(int[] A, int i, int j, int target) {
-        if(i >= j) {
+        if (i >= j) {
             return i - 1;
         }
         int m = (i + j) >> 1;
-        if(A[m] == target) {
+        if (A[m] == target) {
             return search1(A, m + 1, j, target);
-        } else if(A[m] < target) {
+        } else if (A[m] < target) {
             return search1(A, m + 1, j, target);
         } else {
             return search1(A, i, m, target);
