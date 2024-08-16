e677e7617881a4091ac31132adcc9e240834b2e5
jaime.w <463954448@qq.com>
2024-08-16 17:02:33 -0400

    init

--- src/main/java/leetcode/_33/Solution.java
+++ src/main/java/leetcode/_33/Solution.java
@@ -3,9 +3,9 @@ package leetcode._33;
 class Solution {
     public int search(int[] nums, int target) {
         int i = 0, j = nums.length - 1, n = nums.length;
-        for(; i < j;) {
+        for (; i < j; ) {
             int m = (i + j) / 2;
-            if(nums[m] < nums[j]) {
+            if (nums[m] < nums[j]) {
                 j = m;
             } else {
                 i = m + 1;
@@ -13,15 +13,42 @@ class Solution {
         }
 
         int k = i;
-        for(i = 0, j = nums.length - 1; i <= j;) {
+        for (i = 0, j = nums.length - 1; i <= j; ) {
             int m = (i + j) / 2;
             m = (m + k) % n;
-            if(nums[m] == target) {
+            if (nums[m] == target) {
                 return m;
-            } else if(nums[m] < target) {
+            } else if (nums[m] < target) {
                 i = (m + n - k) % n + 1;
             } else {
                 j = (m + n - k) % n - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+class Solution1 {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1, n = nums.length;
+        for (; i < j; ) {
+            int m = (i + j) / 2;
+            if (nums[m] < nums[j]) {
+                j = m;
+            } else {
+                i = m + 1;
+            }
+        }
+
+        int k = i;
+        for (i = 0, j = nums.length; i < j; ) {
+            int m = (i + j) / 2, m1 = (m + k) % n;
+            if (nums[m1] == target) {
+                return m1;
+            } else if (nums[m1] < target) {
+                i = m + 1;
+            } else {
+                j = m;
             }
         }
         return -1;
