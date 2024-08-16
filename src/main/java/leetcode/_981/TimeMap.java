9d480e0c2d7954c8e63a19d9f3a462c482c27465
jaime.w <463954448@qq.com>
2024-08-16 12:44:35 -0400

    init

--- src/main/java/leetcode/_981/TimeMap.java
+++ src/main/java/leetcode/_981/TimeMap.java
@@ -58,3 +58,49 @@ class TimeMap {
  * obj.set(key,value,timestamp);
  * String param_2 = obj.get(key,timestamp);
  */
+
+
+class TimeMap1 {
+
+    HashMap<String, ArrayList<Data>> map = new HashMap();
+
+    class Data {
+        String value;
+        int timestamp;
+
+        public Data(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public void set(String key, String value, int timestamp) {
+        ArrayList<Data> l = map.get(key);
+        if(l == null) {
+            l = new ArrayList();
+        }
+        l.add(new Data(value, timestamp));
+        map.put(key, l);
+    }
+
+    public String get(String key, int timestamp) {
+        ArrayList<Data> l = map.get(key);
+        if(l == null) {
+            return "";
+        }
+        int i = 0, j = l.size();
+        for(; i < j;) {
+            int m = (i + j) / 2;
+            Data d = l.get(m);
+            if(d.timestamp == timestamp) {
+                i = m + 1;
+                break;
+            } else if(d.timestamp > timestamp){
+                j = m;
+            } else {
+                i = m + 1;
+            }
+        }
+        return --i < 0 ? "" : l.get(i).value;
+    }
+}
