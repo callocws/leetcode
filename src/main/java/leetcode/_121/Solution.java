25a094e0dd9b268e59a531d672d35c8ae06703bb
jaime.w <463954448@qq.com>
2024-08-18 17:22:12 -0400

    init

--- src/main/java/leetcode/_121/Solution.java
+++ src/main/java/leetcode/_121/Solution.java
@@ -6,31 +6,31 @@ class Solution {
     public int maxProfit(int[] prices) {
         LinkedList<Integer> l = new LinkedList();
         int m = 0;
-        for(int i = 0; i < prices.length; i++) {
-        	if(l.isEmpty()) {
-        		l.add(prices[i]);
-        	} else if(l.getLast() < prices[i]) {
-        		if(l.size() == 2) {
-        			l.removeLast();
-        		}
-        		l.add(prices[i]);
-        		m = Math.max(l.get(1) - l.get(0), m);
-        	} else if(l.peek() > prices[i]) {
-        		l.clear();
-        		l.add(prices[i]);
-        	}
+        for (int i = 0; i < prices.length; i++) {
+            if (l.isEmpty()) {
+                l.add(prices[i]);
+            } else if (l.getLast() < prices[i]) {
+                if (l.size() == 2) {
+                    l.removeLast();
+                }
+                l.add(prices[i]);
+                m = Math.max(l.get(1) - l.get(0), m);
+            } else if (l.peek() > prices[i]) {
+                l.clear();
+                l.add(prices[i]);
+            }
         }
         return m;
     }
 }
 
 class Solution1 {
-	public int maxProfit(int[] prices) {
-		int m = 0, l = prices[0];
-		for(int i = 1; i < prices.length; i++) {
-			m = Math.max(m, prices[i] - l);
-			l = Math.min(l, prices[i]);
-		}
-		return m;
-	}
+    public int maxProfit(int[] prices) {
+        int m = 0, l = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            m = Math.max(m, prices[i] - l);
+            l = Math.min(l, prices[i]);
+        }
+        return m;
+    }
 }
