63a91fab0e06b3a21feef5302c185b67a0479038
jaime.w <463954448@qq.com>
2024-08-18 16:02:42 -0400

    init

--- src/main/java/leetcode/_76/Solution.java
+++ src/main/java/leetcode/_76/Solution.java
@@ -7,34 +7,33 @@ class Solution {
         HashMap<Character, Integer> h = new HashMap();
         String m = s;
         boolean b = false;
-        for(char c : t.toCharArray()) {
+        for (char c : t.toCharArray()) {
             h.put(c, h.getOrDefault(c, 0) + 1);
         }
 
         char[] cs = s.toCharArray();
-        for(int l = 0, r = 0; r < cs.length; r++) {
+        for (int l = 0, r = 0; r < cs.length; r++) {
             Integer k = h.get(cs[r]);
-            if(k != null) {
+            if (k != null) {
                 h.put(cs[r], k - 1);
             }
-            for(;isValid(h); l++) {
+            for (; isValid(h); l++) {
                 b = true;
                 m = m.length() < r - l + 1 ? m : s.substring(l, r + 1);
 
                 k = h.get(cs[l]);
-                if(k != null) {
+                if (k != null) {
                     h.put(cs[l], k + 1);
                 }
             }
-
         }
 
         return b ? m : "";
     }
 
     boolean isValid(HashMap<Character, Integer> h) {
-        for(int k : h.values()) {
-            if(k > 0) {
+        for (int k : h.values()) {
+            if (k > 0) {
                 return false;
             }
         }
@@ -45,16 +44,16 @@ class Solution {
 class Solution1 {
     public String minWindow(String s, String t) {
         int cnt[] = new int[128];
-        for(char c : t.toCharArray()) {
+        for (char c : t.toCharArray()) {
             cnt[c]++;
         }
         int l, r;
         String m = "";
         char cs[] = s.toCharArray();
-        for(l = r = 0; r < cs.length; r++) {
+        for (l = r = 0; r < cs.length; r++) {
             cnt[cs[r]]--;
-            for(;isValid(cnt); l++) {
-                if(m == "" || m.length() > r - l + 1) {
+            for (; isValid(cnt); l++) {
+                if (m == "" || m.length() > r - l + 1) {
                     m = s.substring(l, r + 1);
                 }
                 cnt[cs[l]]++;
@@ -64,11 +63,44 @@ class Solution1 {
     }
 
     boolean isValid(int cnts[]) {
-        for(int cnt : cnts) {
-            if(cnt > 0) {
+        for (int cnt : cnts) {
+            if (cnt > 0) {
                 return false;
             }
         }
         return true;
+    }
+}
+
+class Solution2 {
+    public String minWindow(String s, String t) {
+        char cs[] = s.toCharArray(), ct[] = t.toCharArray();
+        int count[] = new int[128], result[] = new int[] {0, 1_000_000};
+        for (char c : ct) {
+            count[c]++;
+        }
+        for (int l = 0, r = 0; r < cs.length; r++) {
+            count[cs[r]]--;
+            for (; check(count); ) {
+                if (r - l < result[1] - result[0]) {
+                    result = new int[] {l, r};
+                }
+                count[cs[l++]]++;
+            }
+        }
+        return result[1] - result[0] == 1000_000 ? "" : s.substring(result[0], result[1] + 1);
+    }
+
+    boolean check(int count[]) {
+        for (int cnt : count) {
+            if (cnt > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution2().minWindow("ADOBECODEBANC", "ABC"));
     }
 }
