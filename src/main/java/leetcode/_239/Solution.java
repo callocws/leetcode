fd17b27b71e3a5ca187ac93db51470f3d9ea64da
jaime.w <463954448@qq.com>
2024-08-18 17:08:55 -0400

    init

--- src/main/java/leetcode/_239/Solution.java
+++ src/main/java/leetcode/_239/Solution.java
@@ -6,21 +6,41 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int A[] = new int[nums.length - k + 1];
         LinkedList<Integer> l = new LinkedList();
-        for(int i = 0; i < nums.length; i++) {
-            for(;!l.isEmpty() && nums[l.getLast()] < nums[i];) {
+        for (int i = 0; i < nums.length; i++) {
+            for (; !l.isEmpty() && nums[l.getLast()] < nums[i]; ) {
                 l.removeLast();
             }
             l.add(i);
 
-            if(i - k >= l.peek()) {
+            if (i - k >= l.peek()) {
                 l.pop();
             }
 
             int t = i - k + 1;
-            if(t >= 0) {
+            if (t >= 0) {
                 A[t] = nums[l.peek()];
-            } 
+            }
         }
         return A;
+    }
+}
+
+class Solution1 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int result[] = new int[nums.length - k + 1];
+        LinkedList<Integer> l = new LinkedList();
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (!l.isEmpty() && i - k >= l.peek()) {
+                l.removeFirst();
+            }
+            for (; !l.isEmpty() && nums[l.getLast()] <= nums[i]; ) {
+                l.removeLast();
+            }
+            l.add(i);
+            if (i >= k - 1) {
+                result[j++] = nums[l.peek()];
+            }
+        }
+        return result;
     }
 }
