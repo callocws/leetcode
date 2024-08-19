cd9b3e477ae719120e5b962571df978882c9cfb2
jaime.w <463954448@qq.com>
2024-08-19 14:33:36 -0400

    init

--- src/main/java/leetcode/_143/Solution.java
+++ src/main/java/leetcode/_143/Solution.java
@@ -1,5 +1,6 @@
 package leetcode._143;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import leetcode.ListNode;
 
@@ -7,15 +8,33 @@ class Solution {
     public void reorderList(ListNode head) {
         ListNode p = head, q, t;
         LinkedList<ListNode> l = new LinkedList();
-        for(;p != null; p = p.next) {
+        for (; p != null; p = p.next) {
             l.add(p);
         }
         p = head;
-        for(int i = 0, n = l.size(); i < n / 2; i++) {
+        for (int i = 0, n = l.size(); i < n / 2; i++) {
             t = p.next;
             p.next = q = l.removeLast();
             q.next = t;
             p = t;
+        }
+        p.next = null;
+    }
+}
+
+class Solution1 {
+    public void reorderList(ListNode head) {
+        ListNode h = new ListNode(), p = head;
+        ArrayList<ListNode> l = new ArrayList();
+        for (; p != null; p = p.next) {
+            l.add(p);
+        }
+        p = head;
+        for (int i = 0, n = l.size(); i < (n + 1) / 2; i++) {
+            p = p.next = l.get(i);
+            if (i != n - i - 1) {
+                p = p.next = l.get(n - i - 1);
+            }
         }
         p.next = null;
     }
