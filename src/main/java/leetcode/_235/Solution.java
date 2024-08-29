21998644b0144e3d1fe9f87b19a64e88ac920375
jaime.w <463954448@qq.com>
2024-08-29 13:50:22 -0400

    init

--- src/main/java/leetcode/_235/Solution.java
+++ src/main/java/leetcode/_235/Solution.java
@@ -4,19 +4,34 @@ import leetcode.TreeNode;
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.val > q.val) {
+        if (p.val > q.val) {
             TreeNode t = p;
             p = q;
             q = t;
         }
-        if(root == null) {
+        if (root == null) {
             return null;
         }
-        if(p.val <= root.val && root.val <= q.val) {
+        if (p.val <= root.val && root.val <= q.val) {
             return root;
         }
 
         TreeNode t = lowestCommonAncestor(root.left, p, q);
         return t != null ? t : lowestCommonAncestor(root.right, p, q);
+    }
+}
+
+class Solution1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > q.val) {
+            return lowestCommonAncestor(root, q, p);
+        }
+        if (root.val < p.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
     }
 }
