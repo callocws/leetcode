5ad58549c7d24df37024134b131b0f35683a725d
jaime.w <463954448@qq.com>
2024-09-02 17:55:55 -0400

    init

---
+++ src/main/java/leetcode/_208/Trie1.java
@@ -0,0 +1,110 @@
+package leetcode._208;
+
+import java.util.HashMap;
+
+class Trie {
+
+    class Node {
+        HashMap<Character, Node> m = new HashMap();
+        boolean isComplete;
+    }
+
+    Node head = new Node();
+
+    public Trie() {}
+
+    public void insert(String word) {
+        Node p = head;
+        for (char c : word.toCharArray()) {
+            Node t = p.m.get(c);
+            if (t == null) {
+                t = new Node();
+            }
+            p.m.put(c, t);
+            p = t;
+        }
+        p.isComplete = true;
+    }
+
+    public boolean search(String word) {
+        Node p = head;
+        for (char c : word.toCharArray()) {
+            Node t = p.m.get(c);
+            if (t == null) {
+                return false;
+            }
+            p = t;
+        }
+        return p.isComplete;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node p = head;
+        for (char c : prefix.toCharArray()) {
+            Node t = p.m.get(c);
+            if (t == null) {
+                return false;
+            }
+            p = t;
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such: Trie obj = new Trie();
+ * obj.insert(word); boolean param_2 = obj.search(word); boolean param_3 = obj.startsWith(prefix);
+ */
+class Trie1 {
+
+    class Node {
+        HashMap<Character, Node> map = new HashMap();
+        boolean isComplete;
+
+        void insert(String word) {
+            if (word.length() == 0) {
+                isComplete = true;
+                return;
+            }
+            map.computeIfAbsent(word.charAt(0), k -> new Node()).insert(word.substring(1));
+        }
+
+        public boolean search(String word) {
+            if (word.length() == 0) {
+                return isComplete;
+            }
+            Node t = map.get(word.charAt(0));
+            if (t == null) {
+                return false;
+            }
+            return t.search(word.substring(1));
+        }
+
+        public boolean startsWith(String prefix) {
+            if (prefix.length() == 0) {
+                return true;
+            }
+            Node t = map.get(prefix.charAt(0));
+            if (t == null) {
+                return false;
+            }
+            return t.startsWith(prefix.substring(1));
+        }
+    }
+
+    public Trie1() {}
+
+    Node head = new Node();
+
+    public void insert(String word) {
+        head.insert(word);
+    }
+
+    public boolean search(String word) {
+        return head.search(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        return head.startsWith(prefix);
+    }
+}
