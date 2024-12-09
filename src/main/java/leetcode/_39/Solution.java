981a3887986b3a076d96daffec624c3a4fd010b8
jaime.w <463954448@qq.com>
2024-12-09 17:37:38 -0500

    init

--- src/main/java/leetcode/_39/Solution.java
+++ src/main/java/leetcode/_39/Solution.java
@@ -1,5 +1,6 @@
 package leetcode._39;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,4 +29,27 @@ class Solution {
     }
 }
 
+
+class Solution1 {
+    List<List<Integer>> r = new ArrayList<List<Integer>>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        combine(0, target, candidates, new LinkedList());
+        return r;
+    }
+
+    void combine(int i, int target, int[] candidates, List<Integer> nums) {
+        if(target == 0) {
+            r.add(new ArrayList(nums));
+            return;
+        }
+        if(i >= candidates.length) {
+            return;
+        }
+        nums = new LinkedList(nums);
+        for(int j = 0; target - candidates[i] * j >= 0; j++) {
+            combine(i + 1, target - candidates[i] * j, candidates, nums);
+            nums.add(candidates[i]);
+        }
+    }
+}
 
