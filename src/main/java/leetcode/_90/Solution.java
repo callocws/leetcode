d4857e0db1b17a5acc0c46bd98a220ecc96caf82
jaime.w <463954448@qq.com>
2024-12-09 21:33:26 -0500

    init

--- src/main/java/leetcode/_90/Solution.java
+++ src/main/java/leetcode/_90/Solution.java
@@ -1,9 +1,11 @@
 package leetcode._90;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
     HashSet<List<Integer>> s = new HashSet();
@@ -13,17 +15,41 @@ class Solution {
         traverse(new LinkedList(), nums, 0);
         return new LinkedList(s);
     }
+
     void traverse(List<Integer> r, int[] A, int i) {
         s.add(new LinkedList(r));
-        for(; i < A.length; i++) {
+        for (; i < A.length; i++) {
             r.add(A[i]);
             traverse(r, A, i + 1);
-            r.removeLast();        
+            r.removeLast();
         }
     }
 }
 
 class Solution1 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        return new ArrayList(add(0, nums));
+    }
+
+    Set<List<Integer>> add(int i, int[] nums) {
+        Set<List<Integer>> result = new HashSet();
+        if (i == nums.length) {
+            result.add(new ArrayList());
+        } else {
+            var nextResult = add(i + 1, nums);
+            for (var list : nextResult) {
+                var copy = new ArrayList(list);
+                copy.add(nums[i]);
+                result.add(copy);
+            }
+            result.addAll(nextResult);
+        }
+        return result;
+    }
+}
+
+class Solution2 {
     LinkedList<List<Integer>> l = new LinkedList();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
@@ -31,10 +57,11 @@ class Solution1 {
         traverse(new LinkedList(), nums, 0);
         return l;
     }
+
     void traverse(List<Integer> r, int[] A, int i) {
         l.add(new LinkedList(r));
-        for(int j = i; j < A.length; j++) {
-            if(j > i && A[j] == A[j - 1]) {
+        for (int j = i; j < A.length; j++) {
+            if (j > i && A[j] == A[j - 1]) {
                 continue;
             }
             r.add(A[j]);
