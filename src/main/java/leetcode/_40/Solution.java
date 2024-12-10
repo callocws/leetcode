101d554dbff10080aa358c92d63faf2526321a4a
jaime.w <463954448@qq.com>
2024-12-10 17:25:02 -0500

    init

--- src/main/java/leetcode/_40/Solution.java
+++ src/main/java/leetcode/_40/Solution.java
@@ -2,7 +2,10 @@ package leetcode._40;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
 
@@ -30,3 +33,54 @@ class Solution {
         }
     }
 }
+
+class Solution1 { // timeout
+    Set<List<Integer>> r = new HashSet<List<Integer>>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        combinationSum2(new LinkedList(), 0, candidates, target);
+        return new LinkedList(r);
+    }
+
+    void combinationSum2(List<Integer> list, int i, int[] candidates, int target) {
+        list = new LinkedList(list);
+        if(target == 0) {
+            r.add(list);
+            return;
+        }
+        if(i == candidates.length) {
+            return;
+        }
+        combinationSum2(list, i + 1, candidates, target);
+        list.add(candidates[i]);
+        combinationSum2(list, i + 1, candidates, target - candidates[i]);
+    }
+}
+
+class Solution2 { // timeout
+    Set<List<Integer>> r = new HashSet<List<Integer>>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        combinationSum2(new LinkedList(), 0, candidates, target);
+        return new LinkedList(r);
+    }
+
+    void combinationSum2(List<Integer> list, int i, int[] candidates, int target) {
+        if(target == 0) {
+            r.add(new LinkedList(list));
+            return;
+        }
+        for(int j = i; j < candidates.length; j++) {
+            int t = target - candidates[j];
+            if (t < 0) {
+                return;
+            }
+            list.add(candidates[j]);
+            combinationSum2(list, j + 1, candidates, t);
+            list.removeLast();
+        }
+    }
+}
+
+
+
