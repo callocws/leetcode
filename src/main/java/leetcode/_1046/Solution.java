ebd07c3d59304bba9d6a96cb6067ec4bf9e2c4fb
jaime.w <463954448@qq.com>
2024-12-10 21:09:43 -0500

    init

--- src/main/java/leetcode/_1046/Solution.java
+++ src/main/java/leetcode/_1046/Solution.java
@@ -5,12 +5,12 @@ import java.util.PriorityQueue;
 class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for(int s : stones) {
+        for (int s : stones) {
             pq.add(s);
         }
-        for(;pq.size() >= 2;) {
-            int x = pq.poll(), y = pq.poll(), t = Math.abs(x - y);
-            if(t > 0) {
+        for (; pq.size() >= 2; ) {
+            int x = pq.poll(), y = pq.poll(), t = x - y;
+            if (t > 0) {
                 pq.add(t);
             }
         }
