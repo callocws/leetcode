bd2643cb198af333f1070d14b25ed0b5d4d3229d
jaime.w <463954448@qq.com>
2024-12-14 18:33:43 -0500

    init

--- src/main/java/leetcode/_323/Solution.java
+++ src/main/java/leetcode/_323/Solution.java
@@ -1,4 +1,4 @@
-package neetcode._150.number_of_connected_components_in_an_undirected_graph;
+package leetcode._323;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,15 +8,15 @@ public class Solution {
         int cnt = 0;
         ArrayList<Integer>[] adj = new ArrayList[n];
         boolean visited[] = new boolean[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList();
         }
-        for(int[] e : edges) {
+        for (int[] e : edges) {
             adj[e[0]].add(e[1]);
             adj[e[1]].add(e[0]);
         }
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 cnt++;
                 dfs(visited, adj, i);
             }
@@ -25,39 +25,40 @@ public class Solution {
     }
 
     void dfs(boolean visited[], ArrayList<Integer>[] adj, int i) {
-        if(visited[i]) {
+        if (visited[i]) {
             return;
         }
         visited[i] = true;
-        for(int j : adj[i]) {
+        for (int j : adj[i]) {
             dfs(visited, adj, j);
         }
     }
 }
 
-//union find
+// union find
 class Solution1 {
     public int countComponents(int n, int[][] edges) {
         int[] A = new int[n];
         HashSet<Integer> s = new HashSet();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             A[i] = i;
         }
-        for(int[] e : edges) {
+        for (int[] e : edges) {
             merge(A, e[0], e[1]);
         }
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             s.add(find(A, i));
         }
         return s.size();
     }
 
     int find(int[] A, int i) {
-        if(A[i] != i) {
+        if (A[i] != i) {
             A[i] = find(A, A[i]);
         }
         return A[i];
     }
+
     void merge(int[] A, int i, int j) {
         A[find(A, i)] = find(A, j);
     }
@@ -66,12 +67,12 @@ class Solution1 {
 class Solution2 {
     public int countComponents(int n, int[][] edges) {
         int[] A = new int[n], R = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             A[i] = i;
             R[i] = 1;
         }
-        for(int[] e : edges) {
-            if(merge(A, R, e[0], e[1])) {
+        for (int[] e : edges) {
+            if (merge(A, R, e[0], e[1])) {
                 n--;
             }
         }
@@ -79,7 +80,7 @@ class Solution2 {
     }
 
     int find(int[] A, int i) {
-        if(A[i] != i) {
+        if (A[i] != i) {
             A[i] = find(A, A[i]);
         }
         return A[i];
@@ -88,10 +89,10 @@ class Solution2 {
     boolean merge(int[] A, int[] R, int i, int j) {
         i = find(A, i);
         j = find(A, j);
-        if(i == j) {
+        if (i == j) {
             return false;
         }
-        if(R[i] >= R[j]) {
+        if (R[i] >= R[j]) {
             A[j] = i;
             R[i] += R[j];
         } else {
