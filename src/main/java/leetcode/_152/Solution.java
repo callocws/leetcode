6f1e37ad0ce3e687498e0088993a119606fb1f47
jaime.w <463954448@qq.com>
2024-12-16 21:17:36 -0500

    init

--- src/main/java/leetcode/_152/Solution.java
+++ src/main/java/leetcode/_152/Solution.java
@@ -1,33 +1,23 @@
 package leetcode._152;
 
 class Solution {
-    int product = 0;
     public int maxProduct(int[] nums) {
         int product = nums[0], a, b, i;
-        for(a = b = 1, i = 0; i < nums.length; i++) {
+        for (a = b = 1, i = 0; i < nums.length; i++) {
             int v[] = extreme(a * nums[i], b * nums[i], nums[i]);
             a = v[1];
             b = v[0];
             product = Math.max(product, a);
-            if (product == 1000000000) {
-                break;
-            }
         }
         return product;
     }
 
     public int[] extreme(int... A) {
         int max, min, i;
-        for(max = min = A[0], i = 0; i < A.length; i++) {
+        for (max = min = A[0], i = 0; i < A.length; i++) {
             max = Math.max(max, A[i]);
             min = Math.min(min, A[i]);
         }
-        return new int[]{min, max};
-    }
-
-    public static void main(String[] args) {
-        // [0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0]
-        System.out.println(new Solution().maxProduct(new int[]{0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0}));
-        System.out.println(new Solution().maxProduct(new int[]{2,3,-2,4}));
+        return new int[] {min, max};
     }
 }
