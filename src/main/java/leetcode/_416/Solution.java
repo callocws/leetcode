bb08720b2b34b483650f6d55fc19a39d372e7610
jaime.w <463954448@qq.com>
2024-12-21 16:43:18 -0500

    init

--- src/main/java/leetcode/_416/Solution.java
+++ src/main/java/leetcode/_416/Solution.java
@@ -2,42 +2,43 @@ package leetcode._416;
 
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = 0;
-        for(int i = 0; i < nums.length; i++) {
-            n += nums[i];
+        int s = 0;
+        for (int i = 0; i < nums.length; i++) {
+            s += nums[i];
         }
-        return n % 2 == 0 && f(0, n / 2, nums);
+        return s % 2 == 0 && canPartition(0, s / 2, nums);
     }
-    boolean f(int i, int n, int[] nums) {
-        if(n == 0) {
+
+    boolean canPartition(int i, int target, int[] nums) {
+        if (target == 0) {
             return true;
-        } 
-        if(i >= nums.length || n < 0) {
+        }
+        if (i >= nums.length || target < 0) {
             return false;
         }
-        return f(i + 1, n, nums) || f(i + 1, n - nums[i], nums);
+        return canPartition(i + 1, target, nums) || canPartition(i + 1, target - nums[i], nums);
     }
 }
 
 class Solution1 {
     public boolean canPartition(int[] nums) {
         int n = 0;
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             n += nums[i];
         }
 
-        if(n % 2 == 1) {
+        if (n % 2 == 1) {
             return false;
         }
 
         n >>= 1;
         int[][] q = new int[nums.length + 1][n + 1];
         q[0][n] = 1;
-        for(int i = 1; i <= nums.length; i++) {
-            for(int j = n; j >= 0; j--) {
-                if(q[i - 1][j] == 1) {
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = n; j >= 0; j--) {
+                if (q[i - 1][j] == 1) {
                     q[i][j] = 1;
-                    if(j - nums[i - 1] >= 0) {
+                    if (j - nums[i - 1] >= 0) {
                         q[i][j - nums[i - 1]] = 1;
                     }
                 }
@@ -50,17 +51,17 @@ class Solution1 {
 class Solution2 {
     public boolean canPartition(int[] nums) {
         int n = 0;
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             n += nums[i];
         }
-        if(n % 2 == 1) {
+        if (n % 2 == 1) {
             return false;
         }
         n >>= 1;
         boolean[] q = new boolean[n + 1];
         q[n] = true;
-        for(int num : nums) {
-            for(int j = 0; j <= n - num; j++) {
+        for (int num : nums) {
+            for (int j = 0; j <= n - num; j++) {
                 q[j] = q[j] || q[j + num];
             }
         }
