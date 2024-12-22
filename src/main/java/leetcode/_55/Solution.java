da90dd5309b707e62219c1c392bcf68663deb7c2
jaime.w <463954448@qq.com>
2024-12-22 15:39:21 -0500

    init

--- src/main/java/leetcode/_55/Solution.java
+++ src/main/java/leetcode/_55/Solution.java
@@ -4,9 +4,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         boolean dp[] = new boolean[nums.length];
         dp[0] = true;
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 1; dp[i] && j <= nums[i]; j++) {
-                if(i + j < nums.length) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; dp[i] && j <= nums[i]; j++) {
+                if (i + j < nums.length) {
                     dp[i + j] = true;
                 }
             }
@@ -18,11 +18,25 @@ class Solution {
 class Solution1 {
     public boolean canJump(int[] nums) {
         int j = nums.length - 1;
-        for(int i = j - 1; i >= 0; i--) {
-            if(i + nums[i] >= j) {
+        for (int i = j - 1; i >= 0; i--) {
+            if (i + nums[i] >= j) {
                 j = i;
             }
         }
         return j == 0;
+    }
+}
+
+class Solution2 {
+    public boolean canJump(int[] nums) {
+        int d = nums[0];
+        for (int i = 0; i < nums.length && i <= d; i++) {
+            int t = d;
+            d = Math.max(i + nums[i], d);
+            if (i == d && t == d) {
+                break;
+            }
+        }
+        return d >= nums.length - 1;
     }
 }
