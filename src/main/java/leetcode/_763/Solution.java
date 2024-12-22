a8de9aa43aff3fc7883780f07ce5b18ac55fa47b
jaime.w <463954448@qq.com>
2024-12-22 17:54:57 -0500

    init

--- src/main/java/leetcode/_763/Solution.java
+++ src/main/java/leetcode/_763/Solution.java
@@ -8,16 +8,36 @@ class Solution {
         int A[] = new int[26];
         List<Integer> result = new ArrayList();
         char cs[] = s.toCharArray();
-        for(int i = 0; i < cs.length; i++) {
+        for (int i = 0; i < cs.length; i++) {
             int t = cs[i] - 'a';
             A[t] = Math.max(A[t], i);
         }
-        for(int i = 0, r = A[cs[i] - 'a'], l = i; i < cs.length; i++) {
+        for (int i = 0, r = A[cs[i] - 'a'], l = i; i < cs.length; i++) {
             r = Math.max(r, A[cs[i] - 'a']);
-            if(i == r) {
+            if (i == r) {
                 result.add(r - l + 1);
                 l = r + 1;
             }
+        }
+        return result;
+    }
+}
+
+class Solution2 {
+    public List<Integer> partitionLabels(String s) {
+        int A[] = new int[26];
+        List<Integer> result = new ArrayList();
+        char cs[] = s.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+            int t = cs[i] - 'a';
+            A[t] = Math.max(A[t], i);
+        }
+        for (int i = 0, l, r; i < cs.length; ) {
+            l = r = i;
+            for (; i <= r; i++) {
+                r = Math.max(r, A[cs[i] - 'a']);
+            }
+            result.add(i - l);
         }
         return result;
     }
