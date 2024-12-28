7079397f70810aa1cb82d32e82d403960db64519
jaime.w <463954448@qq.com>
2024-12-27 20:25:59 -0500

    init

--- src/main/java/leetcode/_528/Solution2.java
+++ src/main/java/leetcode/_528/Solution2.java
@@ -6,19 +6,20 @@ class Solution {
     int size;
     Random r;
     int[] w;
+
     public Solution(int[] w) {
         size = w.length;
-        for(int i = 1; i < size; i++) {
+        for (int i = 1; i < size; i++) {
             w[i] += w[i - 1];
         }
         r = new Random();
         this.w = w;
     }
-    
+
     public int pickIndex() {
         int v = r.nextInt(w[size - 1]);
-        for(int i = 0; i < size; i++) {
-            if(v < w[i]) {
+        for (int i = 0; i < size; i++) {
+            if (v < w[i]) {
                 return i;
             }
         }
@@ -30,9 +31,10 @@ class Solution1 {
     int size;
     Random r;
     int[] w;
+
     public Solution1(int[] w) {
         size = w.length;
-        for(int i = 1; i < size; i++) {
+        for (int i = 1; i < size; i++) {
             w[i] += w[i - 1];
         }
         r = new Random();
@@ -42,10 +44,39 @@ class Solution1 {
     public int pickIndex() {
         int v = r.nextInt(w[size - 1]);
         int m, i, j;
-        for(i = 0, j = size - 1; i < j;) {
+        for (i = 0, j = size - 1; i < j; ) {
             m = (i + j) >> 1;
-            if(v < w[m]) {
+            if (v < w[m]) {
                 j = m;
+            } else {
+                i = m + 1;
+            }
+        }
+        return i;
+    }
+}
+
+class Solution2 {
+    int size;
+    Random r;
+    int[] w;
+
+    public Solution2(int[] w) {
+        size = w.length;
+        for (int i = 1; i < size; i++) {
+            w[i] += w[i - 1];
+        }
+        r = new Random();
+        this.w = w;
+    }
+
+    public int pickIndex() {
+        int v = r.nextInt(w[size - 1]);
+        int m, i, j;
+        for (i = 0, j = size - 1; i <= j; ) {
+            m = (i + j) >> 1;
+            if (v < w[m]) {
+                j = m - 1;
             } else {
                 i = m + 1;
             }
