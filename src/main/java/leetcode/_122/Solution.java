fb07b1e5bbb917a526d57a7d20b6ba424497cbc4
jaime.w <463954448@qq.com>
2024-12-28 16:46:23 -0500

    init

---
+++ src/main/java/leetcode/_122/Solution.java
@@ -0,0 +1,14 @@
+package leetcode._122;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - prices[i - 1];
+            if (profit > 0) {
+                sum += profit;
+            }
+        }
+        return sum;
+    }
+}
