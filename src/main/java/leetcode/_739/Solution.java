021bb15c9d798325d20bb00e941499e5b6e08815
jaime.w <463954448@qq.com>
2025-01-02 17:57:56 -0500

    init

--- src/main/java/leetcode/_739/Solution.java
+++ src/main/java/leetcode/_739/Solution.java
@@ -8,10 +8,10 @@ class Solution {
         Stack<Integer> s = new Stack();
         int[] A = new int[temperatures.length];
 
-        for(int i = 0, j = 0; i < temperatures.length; i++) {
-            for(;!s.isEmpty() && temperatures[i] > temperatures[s.peek()];) {
-                int t = s.pop();
-                A[t] = i - t;
+        for (int i = 0; i < temperatures.length; i++) {
+            for (; !s.isEmpty() && temperatures[i] > temperatures[s.peek()]; ) {
+                int j = s.pop();
+                A[j] = i - j;
             }
             s.push(i);
         }
