7bff565427bf60f83d5724490fb9706ef2409a17
jaime.w <463954448@qq.com>
2025-01-05 16:39:42 -0500

    init

--- src/main/java/leetcode/_25/Solution.java
+++ src/main/java/leetcode/_25/Solution.java
@@ -6,27 +6,23 @@ import leetcode.ListNode;
 class Solution {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        LinkedList<ListNode> l = new LinkedList();
-        ListNode p = head, q, h = new ListNode(), t = h, e;
-        for (int i = 0; p != null; p = e) {
-            e = p.next;
-            l.add(p);
-            if (++i == k) {
-                do {
-                    q = l.removeLast();
-                    t.next = q;
-                    t = q;
-                } while (!l.isEmpty());
+        LinkedList<ListNode> list = new LinkedList();
+        ListNode p = head, q, newHead = new ListNode(), t = newHead, next;
+        for (int i = 1; p != null; p = next, i++) {
+            next = p.next;
+            list.add(p);
+            if (i == k) {
+                while (!list.isEmpty()) {
+                    t = t.next = list.removeLast();
+                    t.next = null;
+                }
                 i = 0;
             }
         }
-        t.next = null;
-        for (; !l.isEmpty(); ) {
-            q = l.pop();
-            t.next = q;
-            t = q;
+        if (!list.isEmpty()) {
+            t.next = list.pop();
         }
-        return h.next;
+        return newHead.next;
     }
 }
 
@@ -51,6 +47,33 @@ class Solution1 {
             return head;
         }
         ListNode p = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+
+class Solution1_1 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode p = head, nextHead, newHead;
+        for (int i = 1; p != null && i < k; i++) {
+            p = p.next;
+        }
+        if (p == null) {
+            return head;
+        }
+
+        nextHead = reverseKGroup(p.next, k);
+        newHead = reverse(head, k);
+        head.next = nextHead;
+        return newHead;
+    }
+
+    ListNode reverse(ListNode head, int k) {
+        if (k == 1) {
+            return head;
+        }
+        ListNode p = reverse(head.next, k - 1);
         head.next.next = head;
         head.next = null;
         return p;
