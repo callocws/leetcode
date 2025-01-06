fce63d312fa70500c301f1b431e63b44dcfef46b
jaime.w <463954448@qq.com>
2025-01-06 17:20:45 -0500

    init

--- src/main/java/leetcode/_226/Solution.java
+++ src/main/java/leetcode/_226/Solution.java
@@ -1,16 +1,29 @@
 package leetcode._226;
 
-
 import leetcode.TreeNode;
 
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return null;
         }
         TreeNode l = root.left, r = root.right;
         root.right = invertTree(l);
         root.left = invertTree(r);
+        return root;
+    }
+}
+
+class Solution1 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 }
