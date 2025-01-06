2d866588cf6b380fc3c7124980e7999012efaa6b
jaime.w <463954448@qq.com>
2025-01-06 17:53:57 -0500

    init

--- src/main/java/leetcode/_572/Solution.java
+++ src/main/java/leetcode/_572/Solution.java
@@ -4,7 +4,7 @@ import leetcode.TreeNode;
 
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(isSame(root, subRoot)) {
+        if (isSame(root, subRoot)) {
             return true;
         }
 
@@ -12,13 +12,26 @@ class Solution {
     }
 
     boolean isSame(TreeNode p, TreeNode q) {
-        if(p == null) {
+        if (p == null) {
             return q == null;
         }
 
-        if(q != null) {
+        if (q != null) {
             return p.val == q.val && isSame(p.left, q.left) && isSame(p.right, q.right);
         }
         return false;
+    }
+}
+
+class Solution1 {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return treeString(root).contains(treeString(subRoot));
+    }
+
+    String treeString(TreeNode root) {
+        if (root == null) {
+            return "N";
+        }
+        return String.format("#%s#%s%s", root.val, treeString(root.left), treeString(root.right));
     }
 }
