2ca64a6a131237c0ac88f96c10085c92af48c94c
jaime.w <463954448@qq.com>
2025-01-19 13:17:19 -0500

    init

--- src/main/java/leetcode/_124/Solution.java
+++ src/main/java/leetcode/_124/Solution.java
@@ -4,16 +4,17 @@ import leetcode.TreeNode;
 
 class Solution {
     int m = -1000;
+
     public int maxPathSum(TreeNode root) {
-        f(root);
+        traverse(root);
         return m;
     }
 
-    int f(TreeNode p) {
-        if(p == null) {
+    int traverse(TreeNode p) {
+        if (p == null) {
             return 0;
         }
-        int l = f(p.left), r = f(p.right);
+        int l = traverse(p.left), r = traverse(p.right);
         m = Math.max(m, p.val + Math.max(0, l) + Math.max(0, r));
         return p.val + Math.max(Math.max(0, l), Math.max(0, r));
     }
