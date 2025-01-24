d7458944cc085beaf656abf95446551ca0f6fdc0
jaime.w <463954448@qq.com>
2025-01-24 15:23:21 -0500

    init

--- src/main/java/leetcode/_54/Solution.java
+++ src/main/java/leetcode/_54/Solution.java
@@ -4,21 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    int dirs[][] = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int dirs[][] = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     ArrayList<Integer> result = new ArrayList();
+
     public List<Integer> spiralOrder(int[][] matrix) {
         dfs(matrix, 0, 0, 0);
         return result;
     }
 
     void dfs(int[][] matrix, int i, int j, int d) {
-        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] == 101) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] == 101) {
             return;
         }
         result.add(matrix[i][j]);
         matrix[i][j] = 101;
-        for(int di = d; di != (d - 1 + 4) % 4; di = (di + 1) % 4) {
+        for (int di = d; di != (d - 1 + 4) % 4; di = (di + 1) % 4) {
             dfs(matrix, i + dirs[di][0], j + dirs[di][1], di);
         }
+    }
+}
+
+class Solution1 {
+    int dirs[][] = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    ArrayList<Integer> result = new ArrayList();
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        dfs(matrix, 0, 0, 0);
+        return result;
+    }
+
+    void dfs(int[][] matrix, int i, int j, int d) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] == 101) {
+            return;
+        }
+        result.add(matrix[i][j]);
+        matrix[i][j] = 101;
+        dfs(matrix, i + dirs[d][0], j + dirs[d][1], d);
+        d = (d + 1) % 4;
+        dfs(matrix, i + dirs[d][0], j + dirs[d][1], d);
     }
 }
