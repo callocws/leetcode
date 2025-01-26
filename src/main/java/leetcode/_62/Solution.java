c865b6302f2dfd74da0f9eddafa455a4099179ab
jaime.w <463954448@qq.com>
2025-01-26 17:32:29 -0500

    init

--- src/main/java/leetcode/_62/Solution.java
+++ src/main/java/leetcode/_62/Solution.java
@@ -56,3 +56,34 @@ class Solution1 {
         System.out.println(new Solution1().uniquePaths(59, 5)); // 557845
     }
 }
+
+class Solution_2 {
+    Integer dp[][];
+
+    public int uniquePaths(int m, int n) {
+        dp = new Integer[m][n];
+        return uniquePaths(0, 0, m, n);
+    }
+
+    public int uniquePaths(int i, int j, int m, int n) {
+        if (i == m || j == n) {
+            return 0;
+        }
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        return dp[i][j] = uniquePaths(i + 1, j, m, n) + uniquePaths(i, j + 1, m, n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_2().uniquePaths(3, 3));
+        System.out.println(new Solution_2().uniquePaths(4, 4)); // 20
+        System.out.println(new Solution_2().uniquePaths(10, 10)); // 48620
+        System.out.println(new Solution_2().uniquePaths(23, 12)); // 193536720
+        System.out.println(new Solution_2().uniquePaths(51, 9)); // 1916797311
+        System.out.println(new Solution_2().uniquePaths(59, 5)); // 557845
+    }
+}
