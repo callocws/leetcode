7d8e5a9cfb921548560d0d0bd9e21660fdd33fff
jaime.w <463954448@qq.com>
2025-01-30 16:06:30 -0500

    init

--- src/main/java/leetcode/_97/Solution.java
+++ src/main/java/leetcode/_97/Solution.java
@@ -2,21 +2,26 @@ package leetcode._97;
 
 class Solution {
     Boolean[][][] dp;
+
     public boolean isInterleave(String s1, String s2, String s3) {
         dp = new Boolean[s1.length() + 1][s2.length() + 1][s3.length() + 1];
         return s1.length() + s2.length() == s3.length() && isInterleave(s1, s2, s3, 0, 0, 0);
     }
 
     boolean isInterleave(String s1, String s2, String s3, int i1, int i2, int i3) {
-        if(i1 + i2 == s3.length()) {
+        if (i1 + i2 == s3.length()) {
             return true;
         }
-        if(dp[i1][i2][i3] != null) {
+        if (dp[i1][i2][i3] != null) {
             return dp[i1][i2][i3];
         }
-        if(i1 < s1.length() && s3.charAt(i3) == s1.charAt(i1) && isInterleave(s1, s2, s3, i1 + 1, i2, i3 + 1)) {
+        if (i1 < s1.length()
+                && s3.charAt(i3) == s1.charAt(i1)
+                && isInterleave(s1, s2, s3, i1 + 1, i2, i3 + 1)) {
             return true;
-        } else if(i2 < s2.length() && s3.charAt(i3) == s2.charAt(i2) && isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1)) {
+        } else if (i2 < s2.length()
+                && s3.charAt(i3) == s2.charAt(i2)
+                && isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1)) {
             return true;
         }
         dp[i1][i2][i3] = false;
@@ -26,24 +31,25 @@ class Solution {
 
 class Solution1 {
     Boolean[][] dp;
+
     public boolean isInterleave(String s1, String s2, String s3) {
         dp = new Boolean[s1.length() + 1][s2.length() + 1];
         return s1.length() + s2.length() == s3.length() && isInterleave(s1, s2, s3, 0, 0);
     }
 
     boolean isInterleave(String s1, String s2, String s3, int i1, int i2) {
-        if(i1 + i2 == s3.length()) {
+        if (i1 + i2 == s3.length()) {
             return true;
         }
-        if(dp[i1][i2] != null) {
+        if (dp[i1][i2] != null) {
             return dp[i1][i2];
         }
-        if(i1 < s1.length() && s3.charAt(i1 + i2) == s1.charAt(i1) && isInterleave(s1, s2, s3, i1 + 1, i2)) {
-            return true;
-        } else if(i2 < s2.length() && s3.charAt(i1 + i2) == s2.charAt(i2) && isInterleave(s1, s2, s3, i1, i2 + 1)) {
-            return true;
-        }
-        dp[i1][i2] = false;
-        return false;
+        return dp[i1][i2] =
+                i1 < s1.length()
+                                && s3.charAt(i1 + i2) == s1.charAt(i1)
+                                && isInterleave(s1, s2, s3, i1 + 1, i2)
+                        || i2 < s2.length()
+                                && s3.charAt(i1 + i2) == s2.charAt(i2)
+                                && isInterleave(s1, s2, s3, i1, i2 + 1);
     }
 }
