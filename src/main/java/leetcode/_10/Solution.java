f87e7c5d343e3a6175a1ef9acbc662c9282af3fd
jaime.w <463954448@qq.com>
2025-02-02 14:42:08 -0500

    init

--- src/main/java/leetcode/_10/Solution.java
+++ src/main/java/leetcode/_10/Solution.java
@@ -1,48 +1,50 @@
 package leetcode._10;
 
 import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
     Boolean dp[][] = new Boolean[21][21];
+
     public boolean isMatch(String s, String p) {
-        LinkedList<String> l = new LinkedList();
-        for(char c : p.toCharArray()) {
-            String ns = "" + c;
-            if(c == '*') {
-                ns = l.removeLast() + ns;
+        LinkedList<String> list = new LinkedList();
+        for (char c : p.toCharArray()) {
+            String pattern = "" + c;
+            if (c == '*') {
+                pattern = list.removeLast() + pattern;
             }
-            l.add(ns);
+            list.add(pattern);
         }
-        return isMatch(l.toArray(new String[]{}), s, 0, 0);
+        return isMatch(s, list.toArray(new String[] {}), 0, 0);
     }
 
-    boolean isMatch(String p[], String s, int i, int j) {
-        if(dp[i][j] != null) {
+    boolean isMatch(String s, String[] p, int i, int j) {
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
 
-        if(i == p.length && j == s.length()) {
+        if (j == p.length && i == s.length()) {
             return true;
         }
-        if(i == p.length) {
+        if (j == p.length) {
             return false;
         }
-        if(j == s.length()) {
-            return dp[i][j] =(p[i].length() > 1 && isMatch(p, s, i + 1, j));
+        if (i == s.length()) {
+            return dp[i][j] = (p[j].length() > 1 && isMatch(s, p, i, j + 1));
         }
 
-        if(p[i].length() > 1) {
-            char c = p[i].charAt(0);
-            if(isMatch(p, s, i + 1, j)) {
+        if (p[j].length() > 1) {
+            char c = p[j].charAt(0);
+            if (isMatch(s, p, i, j + 1)) {
                 return dp[i][j] = true;
             }
-            if(c == '.' || c == s.charAt(j)) {
-                return dp[i][j] = isMatch(p, s, i, j + 1) || isMatch(p, s, i + 1, j + 1);
+            if (c == '.' || c == s.charAt(i)) {
+                return dp[i][j] = isMatch(s, p, i + 1, j) || isMatch(s, p, i + 1, j + 1);
             }
         } else {
-            char c = p[i].charAt(0);
-            if(c == '.' || c == s.charAt(j)) {
-                return dp[i][j] = isMatch(p, s, i + 1, j + 1);
+            char c = p[j].charAt(0);
+            if (c == '.' || c == s.charAt(i)) {
+                return dp[i][j] = isMatch(s, p, i + 1, j + 1);
             }
         }
         return dp[i][j] = false;
@@ -53,5 +55,46 @@ class Solution {
         System.out.println(new Solution().isMatch("aaaaaaaaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*"));
 
         System.out.println(new Solution().isMatch("aa", "a"));
+    }
+}
+
+class Solution1 {
+    Boolean dp[][] = new Boolean[21][21];
+
+    public boolean isMatch(String s, String p) {
+        List<String> list = new LinkedList();
+        for (char c : p.toCharArray()) {
+            String pattern = "" + c;
+            if (c == '*') {
+                pattern = list.removeLast() + pattern;
+            }
+            list.add(pattern);
+        }
+        return isMatch(s, list, 0, 0);
+    }
+
+    boolean isMatch(String s, List<String> ps, int i, int j) {
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        if (j == ps.size() && i == s.length()) {
+            return true;
+        }
+        if (j == ps.size()) {
+            return false;
+        }
+        String p = ps.get(j);
+        char c = p.charAt(0);
+        if (i == s.length()) {
+            return dp[i][j] = p.length() > 1 && isMatch(s, ps, i, j + 1);
+        }
+        if (p.length() == 1) {
+            return dp[i][j] = (c == '.' || c == s.charAt(i)) && isMatch(s, ps, i + 1, j + 1);
+        } else {
+            return dp[i][j] =
+                    isMatch(s, ps, i, j + 1)
+                            || (c == '.' || c == s.charAt(i))
+                                    && (isMatch(s, ps, i + 1, j) || isMatch(s, ps, i + 1, j + 1));
+        }
     }
 }
