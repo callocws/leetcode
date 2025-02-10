10bc98eabd3b78d22f067cff21e3800be2448e08
jaime.w <463954448@qq.com>
2025-02-10 18:36:43 -0500

    init

--- src/main/java/leetcode/_289/Solution.java
+++ src/main/java/leetcode/_289/Solution.java
@@ -36,3 +36,53 @@ class Solution {
         return i >= 0 && i < board.length && j >= 0 && j < board[i].length && board[i][j] == 1;
     }
 }
+
+class Solution1 {
+
+    // 0 -> 0
+    // 1 -> 1
+    // 0 1 -> 2
+    // 1 0 -> 3
+
+    public void gameOfLife(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int live = 0;
+                for (int x = -1; x < 2; x++) {
+                    for (int y = -1; y < 2; y++) {
+                        if (valid(board, i + x, j + y)) {
+                            live++;
+                        }
+                    }
+                }
+                if (board[i][j] == 1) {
+                    live--;
+                    if (live < 2 || live > 3) {
+                        board[i][j] = 3;
+                    }
+                } else if (live == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                }
+                if (board[i][j] == 3) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    boolean valid(int[][] board, int i, int j) {
+        return i >= 0
+                && i < board.length
+                && j >= 0
+                && j < board[i].length
+                && (board[i][j] == 1 || board[i][j] == 3);
+    }
+}
