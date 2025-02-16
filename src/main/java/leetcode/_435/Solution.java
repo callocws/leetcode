1c210ffd7a307e574cfb91b625b33286f4713dcc
jaime.w <463954448@qq.com>
2025-02-16 12:02:50 -0500

    init

--- src/main/java/leetcode/_435/Solution.java
+++ src/main/java/leetcode/_435/Solution.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
-        int p[] = intervals[0], cnt = 0;
-        for(int i = 1; i < intervals.length; i++) {
-            int t[] = intervals[i];
-            if(p[1] > t[0]) {
+        int lastInterval[] = intervals[0], cnt = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            int nextInterval[] = intervals[i];
+            if (lastInterval[1] > nextInterval[0]) {
                 cnt++;
-                p = t[1] < p[1] ? t : p;
+                lastInterval = nextInterval[1] < lastInterval[1] ? nextInterval : lastInterval;
             } else {
-                p = t;
+                lastInterval = nextInterval;
             }
         }
         return cnt;
