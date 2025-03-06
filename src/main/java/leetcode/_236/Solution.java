e8f33570594b86ccb9282ef2ed5c81d45a455e18
jaime.w <463954448@qq.com>
2025-03-06 16:32:41 -0500

    init

--- src/main/java/leetcode/_236/Solution.java
+++ src/main/java/leetcode/_236/Solution.java
@@ -31,3 +31,20 @@ class Solution {
         traverse(p, p.right);
     }
 }
+
+class Solution1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
