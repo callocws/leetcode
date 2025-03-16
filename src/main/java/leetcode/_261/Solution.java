ee8399a22525a548b15133e862550008bd78919c
jaime.w <463954448@qq.com>
2025-03-16 13:39:44 -0400

    init

--- src/main/java/leetcode/_261/Solution.java
+++ src/main/java/leetcode/_261/Solution.java
@@ -7,6 +7,34 @@ import java.util.Set;
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
         Set<Integer> visited = new HashSet();
+        Set<Integer> adj[] = new Set[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new HashSet<>();
+        }
+        for (int[] e : edges) {
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
+        }
+        return !hasCycle(visited, adj, 0) && visited.size() == n;
+    }
+
+    boolean hasCycle(Set<Integer> visited, Set<Integer>[] adj, int i) {
+        if (!visited.add(i)) {
+            return true;
+        }
+        for (int j : adj[i]) {
+            adj[j].remove(i);
+            if (hasCycle(visited, adj, j)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Solution1 {
+    public boolean validTree(int n, int[][] edges) {
+        Set<Integer> visited = new HashSet();
         ArrayList<Integer> adj[] = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList();
