deb5b2d42dc6960e4a33f2b099f2f525cebedca8
jaime.w <463954448@qq.com>
2025-03-27 17:21:40 -0400

    init

--- src/main/java/leetcode/_433/Solution.java
+++ src/main/java/leetcode/_433/Solution.java
@@ -1,6 +1,8 @@
 package leetcode._433;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 class Solution {
@@ -40,6 +42,59 @@ class Solution {
 
     public static void main(String[] args) {
         var s = new Solution();
+        System.out.println(s.minMutation("AACCGGTT", "AACCGGTA", new String[] {"AACCGGTA"}));
+        System.out.println(
+                s.minMutation(
+                        "AACCGGTT", "AAACGGTA", new String[] {"AACCGGTA", "AACCGCTA", "AAACGGTA"}));
+    }
+}
+
+class Solution1 {
+    char[] cs = {'A', 'C', 'G', 'T'};
+
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> set = new HashSet(), visitedSet = new HashSet();
+        for (var gene : bank) {
+            set.add(gene);
+        }
+        List<String> list = new ArrayList();
+        list.add(startGene);
+        int cnt = 0;
+        for (int i = 0; i < list.size(); cnt++) {
+            int size = list.size();
+            for (; i < size; i++) {
+                String s = list.get(i);
+                if (s.equals(endGene)) {
+                    return cnt;
+                }
+
+                char[] arr = s.toCharArray();
+                for (int j = 0; j < 8; j++) {
+                    char c = arr[j];
+                    for (int k = 0; k < 4; k++) {
+                        if (arr[j] == cs[k]) {
+                            continue;
+                        }
+                        arr[j] = cs[k];
+                        String e = new String(arr);
+                        if (!set.contains(e)) {
+                            continue;
+                        }
+                        if (!visitedSet.add(e)) {
+                            continue;
+                        }
+                        list.add(e);
+                    }
+                    arr[j] = c;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var s = new Solution1();
         System.out.println(s.minMutation("AACCGGTT", "AACCGGTA", new String[] {"AACCGGTA"}));
         System.out.println(
                 s.minMutation(
