83c7251447e59816c874d8fbde627d4e0aa3a2a0
jaime.w <463954448@qq.com>
2025-04-14 15:19:15 -0400

    init

--- src/main/java/leetcode/_64/Solution.java
+++ src/main/java/leetcode/_64/Solution.java
@@ -21,4 +21,36 @@ class Solution {
         return dp[i][j] =
                 grid[i][j] + Math.min(minPathSum(grid, i + 1, j), minPathSum(grid, i, j + 1));
     }
+
+    void printPathSum(int[][] grid, int i, int j) {
+        if (i >= grid.length || j >= grid[0].length) {
+            return;
+        }
+        System.out.println(grid[i][j] + "->");
+
+        if (minPathSum(grid, i + 1, j) < minPathSum(grid, i, j + 1)) {
+            printPathSum(grid, i + 1, j);
+        } else {
+            printPathSum(grid, i, j + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // 示例输入
+        int[][] grid = {
+            {1, 3, 1},
+            {1, 5, 1},
+            {4, 2, 1}
+        };
+
+        // 计算最小路径和并初始化 dp 表
+        int minSum = solution.minPathSum(grid);
+        System.out.println("最小路径和: " + minSum);
+
+        // 打印最小路径
+        System.out.println("最小路径:");
+        solution.printPathSum(grid, 0, 0);
+    }
 }
